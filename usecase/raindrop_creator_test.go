292a6c6a6e15416863aed770d0b86b8e193651c5
Indra Saputra <flame.fire99@gmail.com>
2021-02-26 15:34:21 +0700

    test NewRaindropCreator

--- usecase/raindrop_creator_test.go
+++ usecase/raindrop_creator_test.go
@@ -1,1 +1,35 @@
 package usecase_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	mock_usecase "github.com/indrasaputra/tetesan-hujan/test/mock/usecase"
+	"github.com/indrasaputra/tetesan-hujan/usecase"
+	"github.com/stretchr/testify/assert"
+)
+
+type RaindropCreator_Executor struct {
+	usecase *usecase.RaindropCreator
+	repo    *mock_usecase.MockRaindropRepository
+}
+
+func TestNewRaindropCreator(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("successfully create an instance of RaindropCreator", func(t *testing.T) {
+		exec := createRaindropCreatorExecutor(ctrl)
+		assert.NotNil(t, exec.usecase)
+	})
+}
+
+func createRaindropCreatorExecutor(ctrl *gomock.Controller) *RaindropCreator_Executor {
+	r := mock_usecase.NewMockRaindropRepository(ctrl)
+	u := usecase.NewRaindropCreator(r)
+
+	return &RaindropCreator_Executor{
+		usecase: u,
+		repo:    r,
+	}
+}
