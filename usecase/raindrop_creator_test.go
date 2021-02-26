1fa8caf6e127655d33c9338c24619b80ac85943d
Indra Saputra <flame.fire99@gmail.com>
2021-02-26 15:39:59 +0700

    test when GetCollections returns error

--- usecase/raindrop_creator_test.go
+++ usecase/raindrop_creator_test.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"testing"
 
+	"github.com/indrasaputra/tetesan-hujan/entity"
+
 	"github.com/golang/mock/gomock"
 	mock_usecase "github.com/indrasaputra/tetesan-hujan/test/mock/usecase"
 	"github.com/indrasaputra/tetesan-hujan/usecase"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,8 +38,27 @@ func TestRaindropCreator_Create(t *testing.T) {
 		err := exec.usecase.Create(context.Background(), nil)
 
 		assert.NotNil(t, err)
-		assert.Contains(t, "Raindrop is nil", err.Error())
+		assert.Contains(t, err.Error(), "Raindrop is nil")
 	})
+
+	t.Run("GetCollections returns error", func(t *testing.T) {
+		exec := createRaindropCreatorExecutor(ctrl)
+
+		exec.repo.EXPECT().GetCollections(context.Background()).Return(nil, errors.New("repository closed"))
+
+		rd := createValidRaindrop()
+		err := exec.usecase.Create(context.Background(), rd)
+
+		assert.NotNil(t, err)
+		assert.Contains(t, err.Error(), "GetCollections returns error")
+	})
+}
+
+func createValidRaindrop() *entity.Raindrop {
+	return &entity.Raindrop{
+		CollectionName: "collection",
+		Link:           "http://raindrop.io",
+	}
 }
 
 func createRaindropCreatorExecutor(ctrl *gomock.Controller) *RaindropCreator_Executor {
