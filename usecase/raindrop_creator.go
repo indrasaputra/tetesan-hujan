35528e2fdf47349c26e9367186362b71f480d929
Indra Saputra <flame.fire99@gmail.com>
2021-02-26 15:22:57 +0700

    create raindrop repository interface

--- usecase/raindrop_creator.go
+++ usecase/raindrop_creator.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"errors"
 
 	"github.com/indrasaputra/tetesan-hujan/entity"
 )
@@ -12,11 +13,33 @@ type CreateRaindrop interface {
 	Create(ctx context.Context, raindrop *entity.Raindrop) error
 }
 
+// RaindropRepository defines the business logic to save a raindrop.
+// It has two methods which are explained in their documentation.
+type RaindropRepository interface {
+	// GetCollections gets all root collections.
+	GetCollections(ctx context.Context) ([]*entity.Collection, error)
+	// SaveRaindrop saves a raindrop.
+	SaveRaindrop(ctx context.Context, raindrop *entity.Raindrop) error
+}
+
 // RaindropCreator responsibles for raindrop creation workflow.
 type RaindropCreator struct {
+	repo RaindropRepository
 }
 
 // NewRaindropCreator creates an instance of RaindropCreator.
-func NewRaindropCreator() *RaindropCreator {
-	return &RaindropCreator{}
+func NewRaindropCreator(repo RaindropRepository) *RaindropCreator {
+	return &RaindropCreator{
+		repo: repo,
+	}
+}
+
+// Create creates a new raindrop.
+// First, it will check whether the collection in which the raindrop will be saved exists.
+// If it exists, raindrop will be saved. Otherwise, it returns error.
+func (rc *RaindropCreator) Create(ctx context.Context, raindrop *entity.Raindrop) error {
+	if raindrop == nil {
+		return errors.New("Raindrop is nil")
+	}
+	return nil
 }
