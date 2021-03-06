ca66917180ab608c2935fc4c85e23bbec6647470
Indra Saputra <flame.fire99@gmail.com>
2021-03-06 13:55:16 +0700

    test NewAPI constructor

--- internal/raindrop/raindrop_test.go
+++ internal/raindrop/raindrop_test.go
@@ -1,1 +1,15 @@
 package raindrop_test
+
+import (
+	"testing"
+
+	"github.com/indrasaputra/tetesan-hujan/internal/raindrop"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewAPI(t *testing.T) {
+	t.Run("successfully create an instance of API", func(t *testing.T) {
+		api := raindrop.NewAPI("http://localhost:8080", "random-token")
+		assert.NotNil(t, api)
+	})
+}
