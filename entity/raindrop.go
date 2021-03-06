fbf10b74dade53bd2faf7feed2b74090aa28257a
Indra Saputra <flame.fire99@gmail.com>
2021-03-06 07:12:59 +0700

    create parsed url struct

--- entity/raindrop.go
+++ entity/raindrop.go
@@ -9,6 +9,24 @@ type Raindrop struct {
 	Link string `json:"link"`
 }
 
+// ParsedURL defines the result of parsed URL from Raindrop.
+type ParsedURL struct {
+	// Result defines the parse URL result.
+	Result bool `json:"result"`
+	// Error defines the parse URL error.
+	Error string `json:"error"`
+	Item  struct {
+		// Title defines the title of the URL.
+		Title string `json:"title"`
+		// Excerpt defines the excerpt of the URL.
+		Excerpt string `json:"excerpt"`
+		// Meta defines the metadata of the item
+		Meta struct {
+			Canonical string `json:"canonical"`
+		} `json:"meta"`
+	} `json:"item"`
+}
+
 // Collection defines the data tyope for a collection.
 type Collection struct {
 	// ID defines the collection's ID.
