52eed4df348249d8edb5336ddad3a7180dd56ac1
Indra Saputra <flame.fire99@gmail.com>
2021-03-06 16:22:04 +0700

    implement logic to handle text message

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -1,16 +1,22 @@
 package telegram
 
 import (
+	"context"
+	"strings"
+
+	"github.com/indrasaputra/tetesan-hujan/entity"
+	"github.com/indrasaputra/tetesan-hujan/usecase"
 	telebot "gopkg.in/tucnak/telebot.v2"
 )
 
 // Bot acts as Telegram Bot.
 type Bot struct {
 	*telebot.Bot
+	bookmarker usecase.CreateBookmark
 }
 
 // NewBot creates an instance of Telegram Bot.
-func NewBot(webhookURL, token string) (*Bot, error) {
+func NewBot(webhookURL, token string, bookmarker usecase.CreateBookmark) (*Bot, error) {
 	webhook := &telebot.Webhook{
 		Endpoint: &telebot.WebhookEndpoint{
 			PublicURL: webhookURL,
@@ -23,5 +29,25 @@ func NewBot(webhookURL, token string) (*Bot, error) {
 	}
 
 	bot, err := telebot.NewBot(setting)
-	return &Bot{bot}, err
+	return &Bot{bot, bookmarker}, err
+}
+
+// Run runs Telegram Bot.
+func (b *Bot) Run() {
+	b.Handle(telebot.OnText, func(message *telebot.Message) {
+		texts := strings.Split(message.Text, " ")
+		if len(texts) != 2 {
+			b.Send(message.Sender, "I only receive text containing collection name and URL")
+			return
+		}
+
+		bookmark := &entity.Bookmark{CollectionName: texts[0], URL: texts[1]}
+		if err := b.bookmarker.Create(context.Background(), bookmark); err != nil {
+			b.Send(message.Sender, "Error on saving bookmark: %s", err.Error())
+			return
+		}
+		b.Send(message.Sender, "Bookmark saved! Visit your raindrop application to see it")
+	})
+
+	b.Start()
 }
